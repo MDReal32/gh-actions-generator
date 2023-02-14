31b961faaea49d9138773a89f71d0a655d1e22a0
Veys Aliyev <veysaliyev00@gmail.com>
2023-02-14 11:53:46 +0400

    WIP: Yaml viewer

--- src/components/ActionView/ActionView.tsx
+++ src/components/ActionView/ActionView.tsx
@@ -1,27 +1,13 @@
 import { FC } from "react";
 import { Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+import { Yaml } from "@utils/Yaml";
 
 import { ActionViewProps } from "./ActionView.props";
-import { GHAction } from "../../types/GHAction";
-import { Yaml } from "../../utils/Yaml";
-
-const actions: GHAction = {
-  name: "Link Checker: All English",
-  on: {
-    push: {
-      branches: ["master"]
-    },
-    pull_request: null
-  }
-};
+import { getJson } from "./redux/selector";
 
 /*
-on:
-  push:
-    branches:
-      - main
-  pull_request:
-
 permissions:
   contents: read
 
@@ -29,54 +15,10 @@ permissions:
 concurrency:
   group: '${{ github.workflow }}@${{ github.event.pull_request.head.label || github.head_ref || github.ref }}'
   cancel-in-progress: true
-
-jobs:
-  check-links:
-    runs-on: ${{ fromJSON('["ubuntu-latest", "self-hosted"]')[github.repository == 'github/docs-internal'] }}
-    steps:
-      - name: Checkout
-        uses: actions/checkout@v3
-
-      - name: Setup node
-        uses: actions/setup-node@v3
-        with:
-          node-version: 16.13.x
-          cache: npm
-
-      - name: Install
-        run: npm ci
-
-
-      - name: Gather files changed
-        uses: trilom/file-changes-action@a6ca26c14274c33b15e6499323aac178af06ad4b
-        with:
-          fileOutput: 'json'
-
-
-      - name: Show files changed
-        run: cat $HOME/files.json
-
-      - name: Link check (warnings, changed files)
-        run: |
-          ./script/rendered-content-link-checker.mjs \
-            --language en \
-            --max 100 \
-            --check-anchors \
-            --check-images \
-            --verbose \
-            --list $HOME/files.json
-
-      - name: Link check (critical, all files)
-        run: |
-          ./script/rendered-content-link-checker.mjs \
-            --language en \
-            --exit \
-            --verbose \
-            --check-images \
-            --level critical*/
+*/
 
 export const ActionView: FC<ActionViewProps> = () => {
-  const ghAction = actions;
+  const ghAction = useSelector(getJson);
   const yml = Yaml.parse(ghAction);
 
   return (

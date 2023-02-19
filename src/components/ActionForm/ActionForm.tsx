92f76af8c4f21c02ea2820004481f8b4e980553f
Veys Aliyev <veysaliyev00@gmail.com>
2023-02-19 02:21:47 -0500

    add initial job

--- src/components/ActionForm/ActionForm.tsx
+++ src/components/ActionForm/ActionForm.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Form, Stack } from "react-bootstrap";
 import * as yup from "yup";
 
 import { AddableList } from "@components/AddableList";
 import { Input } from "@components/Input";
 import { setName } from "@components/ActionView/redux/reducer";
 
-import { getBranches } from "./redux/selectors";
-import { setBranches } from "./redux/reducer";
-import { BranchRenderer } from "@components/ActionForm/components/BranchRenderer";
+import { getBranches, setBranches } from "./redux";
+import { ghRunners } from "./common/constants";
+import { BranchRenderer, JobRenderer, StepRenderer } from "./components";
 
 export const ActionForm: FC = () => {
   const dispatch = useDispatch();
@@ -24,12 +25,48 @@ export const ActionForm: FC = () => {
         label="Branches"
         isSetId
         onChange={branches => dispatch(setBranches(branches))}
+        btnLabel="Add Branch"
       >
         <Input
           id="name"
           placeholder="Enter branch name"
           schema={yup.string().required("Branch is required")}
         />
+      </AddableList>
+
+      <AddableList
+        render={JobRenderer}
+        list={branches}
+        label="Jobs"
+        isSetId
+        onChange={branches => dispatch(setBranches(branches))}
+        btnLabel="Add Job"
+      >
+        <Stack direction="horizontal" gap={2}>
+          <Input
+            id="name"
+            placeholder="Enter job name"
+            schema={yup.string().required("Branch is required")}
+          />
+
+          <Form.Select defaultValue="0">
+            <option disabled>Choose...</option>
+
+            {Object.entries(ghRunners).map(([type, label]) => (
+              <option key={type} value={type}>
+                {label}
+              </option>
+            ))}
+          </Form.Select>
+        </Stack>
+
+        <AddableList list={[]} render={StepRenderer} label="Steps" btnLabel="Add Step">
+          <Input
+            id="name"
+            placeholder="Enter step name"
+            schema={yup.string().required("Branch is required")}
+          />
+        </AddableList>
       </AddableList>
     </div>
   );
